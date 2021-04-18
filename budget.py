05d3f5afa41b618e4d702ced0cc85b93b0124554
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-04-18 17:51:38 +0100

    added budget class

---
+++ budget.py
@@ -0,0 +1,21 @@
+class Budget(object):
+	"""docstring for Budget"""
+	def __init__(self, category, amount):
+		super(Budget, self).__init__()
+		self.category = category
+		self.amount = amount
+		
+	def transfer(self, budget2, transfer_amount):
+		if transfer_amount > self.amount:
+			return "Cannot make this transaction"
+		self.amount -= transfer_amount
+		budget2.amount += transfer_amount
+		return "transfer succesful"
+
+
+clothes_budget = Budget('clothes', 500)
+food_budget = Budget('food', 200)
+print(clothes_budget.amount)
+clothes_budget.transfer(food_budget, 300)
+print(clothes_budget.amount)
+print(food_budget.amount)
